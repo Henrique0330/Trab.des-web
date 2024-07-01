fbcb623ee6c19e0bdd01c58e6d234ea1acf02e06
Henrique0330 <henriquebelboni@gmail.com>
2024-07-01 15:07:09 -0300

    começo de um sonho

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+
+
+</script>
+
+<template>
+  <h1 class="Tprincipal">Spotify</h1>
+
+</template>
+<style >
+  body {
+    background-color: black;
+  }
+ .Tprincipal{
+  color: white;
+  margin-bottom: 450px;
+  
+ }
+</style>
